c1e01d8346199bed082ba07735886a410e784c61
Sinankarakurt <asilsinankarakurt@gmail.com>
2024-07-16 17:46:29 +0300

    Model/Bashekim.java sayfası oluşturuldu.

---
+++ HastaneOtomasyon/src/Model/Bashekim.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Bashekim extends User 
+{
+
+	public Bashekim(int id, String tcno, String name, String pass, String type)
+	{
+		super(id, tcno, name, pass, type);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
