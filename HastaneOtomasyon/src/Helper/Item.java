45379681ef5630c1fc2058c78b068c3b55961c75
asils <asils@DESKTOP-QJR10VG>
2024-08-14 08:46:02 +0300

    comboxtaki doktor isimlerinin id lerini ve isimlerini çekmek için Helper/Item key ve valu değişkenleri oluşturuldu.(8:20)

---
+++ HastaneOtomasyon/src/Helper/Item.java
@@ -0,0 +1,41 @@
+package Helper;
+
+public class Item
+{
+	private int key;
+	private String value;
+	//-------------------------------------------
+	public Item(int key, String value) {
+		
+		this.key = key;
+		this.value = value;
+	}
+
+	//----------------------
+	public Item() {
+		super();
+	}
+
+	//-----------------------------------
+	
+
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	//-----------------------------------------------
+	
+
+}
