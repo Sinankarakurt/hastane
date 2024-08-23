0f21bd82895a22e014cc00d0b281e487dedc9ad8
asils <asils@DESKTOP-QJR10VG>
2024-08-23 16:13:03 +0300

    Model/Doctor.java da doctor çalışma saatlari addWhour fonksiyonu oluşturuldu.(19:00)

--- HastaneOtomasyon/src/Model/Doctor.java
+++ HastaneOtomasyon/src/Model/Doctor.java
@@ -3,6 +3,7 @@ package Model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Doctor extends User
@@ -25,7 +26,49 @@ public class Doctor extends User
 	}
 //-------------------------------------------------------------------
 	
+	public boolean addWhour(int doctor_id,String doctor_name,String wdate) throws SQLException
+	{
+		int key=0;
+		int count=0;
+		String query="INSERT INTO whour ( doctor_id,doctor_name,wdate) 	VALUES (?,?,?) ";
+		
+		try 
+		{
+			st=con.createStatement();
+			
+	rs=st.executeQuery("SELECT * FROM whour WHERE status='a'AND doctor_id="+doctor_id+" AND wdate='"+wdate+"'");
 	
+			while (rs.next())
+			{
+				count++;
+				break;
+				
+			}
+			if (count==0)
+			{
+				
+			
+			preparedStatement=con.prepareStatement(query);
+			preparedStatement.setInt(1,doctor_id);
+			preparedStatement.setString(2, doctor_name);
+			preparedStatement.setString(3, wdate);
+			preparedStatement.executeUpdate();
+			}
+				key=1;
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		if (key==1)
+			
+			return true;
+		else
+			return false;
+		
+		
+	}
 
 	
 }
