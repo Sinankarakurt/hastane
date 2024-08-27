c6d58c4fdc5013d77915847a59cc5deabe3a67f6
asils <asils@DESKTOP-QJR10VG>
2024-08-27 13:51:56 +0300

    hastaGUI oluşturuldu(17:00)

--- HastaneOtomasyon/src/HastaGUI.java
+++ HastaneOtomasyon/src/HastaGUI.java
@@ -4,10 +4,19 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import Model.Hasta;
+
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTabbedPane;
+
 public class HastaGUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	private JPanel w_pane;
+	private static Hasta hasta=new Hasta();
 
 	/**
 	 * Launch the application.
@@ -16,7 +25,7 @@ public class HastaGUI extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					HastaGUI frame = new HastaGUI();
+					HastaGUI frame = new HastaGUI(hasta);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -28,13 +37,26 @@ public class HastaGUI extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public HastaGUI() {
+	public HastaGUI(Hasta hasta) {
+		setResizable(false);
+		setTitle("Hastane Yönetim Sistemi");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setBounds(100, 100, 750, 500);
+		w_pane = new JPanel();
+		w_pane.setBackground(Color.WHITE);
+		w_pane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
+		setContentPane(w_pane);
+		w_pane.setLayout(null);
+		
+		JButton btn_cikis = new JButton("Çıkış");
+		btn_cikis.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		btn_cikis.setBounds(585, 11, 110, 26);
+		w_pane.add(btn_cikis);
+		
+		JLabel lbl_bashekimName = new JLabel("Hoşgeliniz Sayın   : "+hasta.getName());
+		lbl_bashekimName.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		lbl_bashekimName.setBounds(70, 11, 198, 26);
+		w_pane.add(lbl_bashekimName);
 	}
-
 }
